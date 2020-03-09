d4f19915dd94085b069e9a75de810e4295c4a9c9
Jiren Patel <jirenpatel@gmail.com>
2020-03-09 18:35:47 +0530

    schema migration: add test for index column

---
+++ test/spanner_activerecord/index/column_test.rb
@@ -0,0 +1,53 @@
+require "test_helper"
+
+describe SpannerActiverecord::Table::Column, :mock_spanner_activerecord  do
+  let(:table_name) { "test-table" }
+  let(:column_name) { "test-column" }
+  let(:index_name) { "test-index"}
+
+  describe "#new" do
+    it "create a instance of index column" do
+      column = SpannerActiverecord::Index::Column.new(
+        table_name, index_name, column_name,
+        order: "DESC", ordinal_position: 1, connection: connection
+      )
+
+      column.name.must_equal column_name
+      column.table_name.must_equal table_name
+      column.index_name.must_equal index_name
+      column.order.must_equal "DESC"
+      column.desc?.must_equal true
+      column.ordinal_position.must_equal 1
+      column.storing?.must_equal false
+    end
+  end
+
+  describe "#stroing?" do
+    it "set storing for index column" do
+      column = SpannerActiverecord::Index::Column.new(
+        table_name, index_name, column_name,
+      )
+
+      column.storing?.must_equal true
+
+      column = SpannerActiverecord::Index::Column.new(
+        table_name, index_name, column_name, ordinal_position: 1
+      )
+
+      column.storing?.must_equal false
+    end
+  end
+
+  describe "#desc!" do
+    it "set DESC order" do
+      column = SpannerActiverecord::Index::Column.new(
+        table_name, index_name, column_name
+      )
+      column.order.must_be :nil?
+      column.desc?.must_equal false
+      column.desc!
+      column.desc?.must_equal true
+      column.order.must_equal "DESC"
+    end
+  end
+end
