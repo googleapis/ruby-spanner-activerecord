03a6b29adfad2b6002c2b36174042e71ea313817
Jiren Patel <jirenpatel@gmail.com>
2020-04-06 11:40:27 +0530

    fix string escape chars and timestamp casting

--- lib/active_record/type/spanner/time.rb
+++ lib/active_record/type/spanner/time.rb
@@ -5,8 +5,18 @@ module ActiveRecord
     module Spanner
       class Time < ActiveRecord::Type::Time
         def serialize value
-          value = super value
           value.acts_like?(:time) ? value.utc.rfc3339(9) : value
+        end
+
+        def user_input_in_time_zone value
+          return value.in_time_zone if value.is_a? ::Time
+          super value
+        end
+
+        private
+
+        def cast_value value
+          value.is_a?(::String) ? ::Time.parse(value) : value
         end
       end
     end
