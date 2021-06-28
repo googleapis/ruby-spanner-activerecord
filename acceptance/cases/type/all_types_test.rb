2ac9c0f34c6c98f3d9a645f4dd503b08b83753fb
Knut Olav Løite <koloite@gmail.com>
2021-06-28 11:32:45 +0200

    test: add additional tests + fix readme

--- acceptance/cases/type/all_types_test.rb
+++ acceptance/cases/type/all_types_test.rb
@@ -26,23 +26,27 @@ module ActiveRecord
         end
       end
 
+      def create_test_record
+        AllTypes.create col_string: "string", col_int64: 100, col_float64: 3.14, col_numeric: 6.626, col_bool: true,
+          col_bytes: StringIO.new("bytes"), col_date: ::Date.new(2021, 6, 23),
+          col_timestamp: ::Time.new(2021, 6, 23, 17, 8, 21, "+02:00"),
+          col_array_string: ["string1", nil, "string2"],
+          col_array_int64: [100, nil, 200],
+          col_array_float64: [3.14, nil, 2.0/3.0],
+          col_array_numeric: [6.626, nil, 3.20],
+          col_array_bool: [true, nil, false],
+          col_array_bytes: [StringIO.new("bytes1"), nil, StringIO.new("bytes2")],
+          col_array_date: [::Date.new(2021, 6, 23), nil, ::Date.new(2021, 6, 24)],
+          col_array_timestamp: [::Time.new(2021, 6, 23, 17, 8, 21, "+02:00"), nil, \
+                                ::Time.new(2021, 6, 24, 17, 8, 21, "+02:00")]
+      end
+
       def test_create_record
         [nil, :serializable, :buffered_mutations].each do |isolation|
           initial_count = AllTypes.count
           record = nil
           run_in_transaction isolation do
-            record = AllTypes.create col_string: "string", col_int64: 100, col_float64: 3.14, col_numeric: 6.626, col_bool: true,
-                            col_bytes: StringIO.new("bytes"), col_date: ::Date.new(2021, 6, 23),
-                            col_timestamp: ::Time.new(2021, 6, 23, 17, 8, 21, "+02:00"),
-                            col_array_string: ["string1", nil, "string2"],
-                            col_array_int64: [100, nil, 200],
-                            col_array_float64: [3.14, nil, 2.0/3.0],
-                            col_array_numeric: [6.626, nil, 3.20],
-                            col_array_bool: [true, nil, false],
-                            col_array_bytes: [StringIO.new("bytes1"), nil, StringIO.new("bytes2")],
-                            col_array_date: [::Date.new(2021, 6, 23), nil, ::Date.new(2021, 6, 24)],
-                            col_array_timestamp: [::Time.new(2021, 6, 23, 17, 8, 21, "+02:00"), nil, \
-                                          ::Time.new(2021, 6, 24, 17, 8, 21, "+02:00")]
+            record = create_test_record
           end
 
           # Verify that the record was created and that the data can be read back.
@@ -70,6 +74,78 @@ module ActiveRecord
                             nil, \
                             ::Time.new(2021, 6, 24, 17, 8, 21, "+02:00")].map { |timestamp| timestamp&.utc },
                        record.col_array_timestamp.map { |timestamp| timestamp&.utc}
+        end
+      end
+
+      def test_update_record
+        [nil, :serializable, :buffered_mutations].each do |isolation|
+          # First create a test record outside a transaction.
+          record = create_test_record
+
+          run_in_transaction isolation do
+            # Update the record in a transaction using different isolation levels.
+            record.update col_string: "new string", col_int64: 200, col_float64: 6.28, col_numeric: 10.1,
+                          col_bool: false, col_bytes: StringIO.new("new bytes"),
+                          col_date: ::Date.new(2021, 6, 28),
+                          col_timestamp: ::Time.new(2021, 6, 28, 11, 22, 21, "+02:00"),
+                          col_array_string: ["new string 1", "new string 2"],
+                          col_array_int64: [300, 200, 100],
+                          col_array_float64: [1.1, 2.2, 3.3],
+                          col_array_numeric: [3.3, 2.2, 1.1],
+                          col_array_bool: [false, true, false],
+                          col_array_bytes: [StringIO.new("new bytes 1"), StringIO.new("new bytes 2")],
+                          col_array_date: [::Date.new(2021, 6, 28)],
+                          col_array_timestamp: [::Time.new(2020, 12, 31, 0, 0, 0, "utc")]
+          end
+
+          # Verify that the record was updated.
+          record = AllTypes.find record.id
+          assert_equal "new string", record.col_string
+          assert_equal 200, record.col_int64
+          assert_equal 6.28, record.col_float64
+          assert_equal 10.1, record.col_numeric
+          assert_equal false, record.col_bool
+          assert_equal StringIO.new("new bytes").read, record.col_bytes.read
+          assert_equal ::Date.new(2021, 6, 28), record.col_date
+          assert_equal ::Time.new(2021, 6, 28, 11, 22, 21, "+02:00").utc, record.col_timestamp.utc
+
+          assert_equal ["new string 1", "new string 2"], record.col_array_string
+          assert_equal [300, 200, 100], record.col_array_int64
+          assert_equal [1.1, 2.2, 3.3], record.col_array_float64
+          assert_equal [3.3, 2.2, 1.1], record.col_array_numeric
+          assert_equal [false, true, false], record.col_array_bool
+          assert_equal [StringIO.new("new bytes 1"), StringIO.new("new bytes 2")].map(&:read),
+                       record.col_array_bytes.map(&:read)
+          assert_equal [::Date.new(2021, 6, 28)], record.col_array_date
+          assert_equal [::Time.new(2020, 12, 31, 0, 0, 0, "utc")].map(&:utc),
+                       record.col_array_timestamp.map(&:utc)
+        end
+      end
+
+      def test_create_empty_arrays
+        [nil, :serializable, :buffered_mutations].each do |isolation|
+          record = nil
+          run_in_transaction isolation do
+            record = AllTypes.create \
+              col_array_string: [],
+              col_array_int64: [],
+              col_array_float64: [],
+              col_array_numeric: [],
+              col_array_bool: [],
+              col_array_bytes: [],
+              col_array_date: [],
+              col_array_timestamp: []
+          end
+
+          record = AllTypes.find record.id
+          assert_equal [], record.col_array_string
+          assert_equal [], record.col_array_int64
+          assert_equal [], record.col_array_float64
+          assert_equal [], record.col_array_numeric
+          assert_equal [], record.col_array_bool
+          assert_equal [], record.col_array_bytes
+          assert_equal [], record.col_array_date
+          assert_equal [], record.col_array_timestamp
         end
       end
     end
