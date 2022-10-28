04e074d3bef3b922f8888e4a773b166eced2c367
Knut Olav Løite <koloite@gmail.com>
2022-10-28 12:10:32 +0200

    feat: support column DEFAULT expressions in migrations
    Fixes #193

--- test/migrations_with_mock_server/migrations_with_mock_server_test.rb
+++ test/migrations_with_mock_server/migrations_with_mock_server_test.rb
@@ -965,6 +965,27 @@ module TestMigrationsWithMockServer
       assert_equal expectedDdl, ddl_requests[2].statements[0]
     end
 
+    def test_create_table_with_default_value
+      context = ActiveRecord::MigrationContext.new(
+        "#{Dir.pwd}/test/migrations_with_mock_server/db/migrate",
+        ActiveRecord::SchemaMigration
+      )
+
+      register_version_result "1", "11"
+
+      context.migrate 11
+
+      ddl_requests = @database_admin_mock.requests.select { |req| req.is_a?(Google::Cloud::Spanner::Admin::Database::V1::UpdateDatabaseDdlRequest) }
+      # The migration simulation also creates the two migration metadata tables.
+      assert_equal 3, ddl_requests.length
+      assert_equal 1, ddl_requests[2].statements.length
+
+      expectedDdl = "CREATE TABLE `singers` "
+      expectedDdl << "(`id` INT64 NOT NULL, `name` STRING(MAX) NOT NULL DEFAULT 'no name', `age` INT64 NOT NULL DEFAULT 0) "
+      expectedDdl << "PRIMARY KEY (`id`)"
+      assert_equal expectedDdl, ddl_requests[2].statements[0]
+    end
+
     def register_schema_migrations_table_result
       sql = "SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, PARENT_TABLE_NAME, ON_DELETE_ACTION FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='' AND TABLE_NAME='schema_migrations'"
       register_empty_select_tables_result sql
