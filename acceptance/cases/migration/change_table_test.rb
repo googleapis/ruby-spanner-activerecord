40a43fe3eb330e03a275b1c1d4e91cd4a77b6257
Nivedha <nivedhasenthil@gmail.com>
2022-12-22 11:22:31 +0530

    Update change_table_test.rb

--- acceptance/cases/migration/change_table_test.rb
+++ acceptance/cases/migration/change_table_test.rb
@@ -86,14 +86,14 @@ module ActiveRecord
 
       def test_index_exists
         with_change_table do |t|
-          @connection.expect :index_exists?, nil, [:delete_me, :bar]
+          @connection.expect :index_exists?, nil, [:delete_me, :bar], {}
           t.index_exists?(:bar)
         end
       end
 
       def test_index_exists_with_options
         with_change_table do |t|
-          @connection.expect :index_exists?, nil, [:delete_me, :bar], **{unique: true}
+          @connection.expect :index_exists?, nil, [:delete_me, :bar], {unique: true}
           t.index_exists?(:bar, unique: true)
         end
       end
