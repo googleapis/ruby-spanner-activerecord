880716c0504c49630472433a073240dcbf1235e6
nownabe <nownabe@users.noreply.github.com>
2023-05-08 14:54:13 +0900

    fix: use original types for composite primary keys
    Close #245

--- acceptance/cases/models/interleave_test.rb
+++ acceptance/cases/models/interleave_test.rb
@@ -7,6 +7,7 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "test_helpers/with_separate_database"
 require "models/singer"
 require "models/album_partial_disabled"
 
@@ -30,6 +31,42 @@ module ActiveRecord
 
       def test_with_partial_inserts_disabled
         AlbumPartialDisabled.create! title: "Title3", singer: singer
+      end
+
+      class StringParentKeyTest < SpannerAdapter::TestCase
+        include TestHelpers::WithSeparateDatabase
+
+        class Singer < ActiveRecord::Base
+          has_many :albums, foreign_key: :singer_id
+        end
+
+        class Album < ActiveRecord::Base
+          self.primary_keys = [:singer_id, :album_id]
+          belongs_to :singer, foreign_key: :singer_id
+        end
+
+        def setup
+          super
+
+          connection.ddl_batch do
+            connection.create_table :singers, id: false do |t|
+              t.string :singer_id, limit: 36, primary_key: true, null: false
+              t.string :name, null: false
+            end
+
+            connection.create_table :albums, id: false do |t|
+              t.interleave_in :singers
+              t.string :singer_id, limit: 36, parent_key: true, primary_key: true, null: false
+              t.integer :album_id, primary_key: true, null: false
+              t.string :title, null: false
+            end
+          end
+        end
+
+        def test_create_album
+          singer = Singer.create!(id: SecureRandom.uuid, name: "a singer")
+          Album.create!(singer: singer, title: "an album")
+        end
       end
     end
   end
---
+++ acceptance/test_helpers/with_separate_database.rb
@@ -0,0 +1,41 @@
+require "securerandom"
+
+require "active_record/tasks/spanner_database_tasks"
+
+module TestHelpers
+  module WithSeparateDatabase
+    attr_reader :connection
+
+    def setup
+      spanner_adapter_connection.create_database
+      ActiveRecord::Base.establish_connection connection_config
+      @connection = ActiveRecord::Base.connection
+    end
+
+    def teardown
+      spanner_adapter_connection.database.drop
+    end
+
+    def connection_config
+      {
+        "adapter" => "spanner",
+        "emulator_host" => ENV["SPANNER_EMULATOR_HOST"],
+        "project" => ENV["SPANNER_TEST_PROJECT"],
+        "instance" => ENV["SPANNER_TEST_INSTANCE"],
+        "credentials" => ENV["SPANNER_TEST_KEYFILE"],
+        "database" => database_id,
+      }
+    end
+
+    def database_id
+      @database_id ||= "ar-test-#{SecureRandom.hex 4}"
+    end
+
+    def spanner_adapter_connection
+      @spanner_adapter_connection ||=
+        ActiveRecordSpannerAdapter::Connection.new(
+          connection_config.symbolize_keys
+        )
+    end
+  end
+end
