8ec4c8ebd06a13882ed958bbb9e7daeb023438d6
Knut Olav Løite <koloite@gmail.com>
2024-12-12 13:57:37 +0100

    fix: only add methods for 7.2 and higher

--- lib/active_record/connection_adapters/spanner/schema_statements.rb
+++ lib/active_record/connection_adapters/spanner/schema_statements.rb
@@ -23,6 +23,7 @@ module ActiveRecord
       module SchemaStatements
         VERSION_6_1_0 = Gem::Version.create "6.1.0"
         VERSION_6_0_3 = Gem::Version.create "6.0.3"
+        VERSION_7_2 = Gem::Version.create "7.2.0"
 
         def current_database
           @connection.database_id
@@ -401,12 +402,14 @@ module ActiveRecord
           information_schema { |i| i.check_constraints table_name }
         end
 
-        def migration_context
-          pool.migration_context
-        end
+        if ActiveRecord.gem_version >= VERSION_7_2
+          def migration_context
+            pool.migration_context
+          end
 
-        def schema_migration
-          pool.schema_migration
+          def schema_migration
+            pool.schema_migration
+          end
         end
 
         def assume_migrated_upto_version version
